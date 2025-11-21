47978d3f9f05f8b0108c1ccdf173b4c6ac3a6531
won <won@wonui-MacBookPro.local>
2025-11-21 12:03:50 +0900

    Remove all files, keep only Revenue Data and Building a Dashboard.ipynb

--- f.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-# Download historical data for a stock
-AMD = yf.Ticker("AMD")
-AMD_info = AMD.info
-# print(AMD_info)
-AMD_sector = AMD_info['sector']
-# print(AMD_sector)
-AMD_price = AMD_info['currentPrice']
-print(AMD_price)
